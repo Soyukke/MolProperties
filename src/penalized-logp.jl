60b0626dc6fd9666ecefb19956f8c29d6a2cd17e
soyukke <soyukke@gmail.com>
2020-10-01 18:52:50 +0900

    add penalized_logp

--- src/penalized-logp.jl
+++ src/penalized-logp.jl
@@ -1,3 +1,31 @@
 """
 Calculate Penalized LogP by using RDKit package
 """
+
+using PyCall
+using Printf
+
+struct RDKit
+    molecules::PyObject
+end
+
+function init()
+    push!(pyimport("sys")["path"], @__DIR__)
+    molecules = pyimport("molecules")
+    return RDKit(molecules)
+end
+
+function main()
+    println("hello")
+end
+
+"""
+    penalized_logp()
+
+`smilesstr` is Smiles string.
+return penalized LogP value of `smilesstr` molecule.
+"""
+function penalized_logp(rdkit::RDKit, smilesstr::String)
+    mol = rdkit.molecules.smiles_to_mol(smilesstr)
+    return rdkit.molecules.penalized_logp(mol)
+end
